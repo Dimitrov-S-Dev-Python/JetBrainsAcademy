514ac475741eeb04dd124d2ef276ba338593538c
Stan <dimitrov.s.dev@gmail.com>
2023-02-12 11:40:06 +0200

    Honest_Calculator

--- Honest_Calculator/Salutions.py
+++ Honest_Calculator/Salutions.py
@@ -233,9 +233,32 @@ print("""
 ' '' '''
 ' '' '''
 """)
+-------------------------------------------
+Suppose you want to carry a box with dimensions A × B × C (length × width × height) through the doorway with dimensions X × Y (width × height). 
+Write a program to check whether it is possible.
+
+The input format:
+The input comprises five strings with numbers in the following order:
+ A, B, C, X, Y. If the size of the doorway is greater than or equal 
+ to any two dimensions of the box, 
+ it is considered that the box can be carried.
+
+The output format:
+If the box can be carried through the doorway, output
+"The box can be carried".
+If it cannot be carried, output 
+"The box cannot be carried".
+
+a = int(input())
+b = int(input())
+c = int(input())
+x = int(input())
+y = int(input())
+
+if (b + c) < (x + y):
+    print("The box can be carried")
+else:
+    print("The box cannot be carried")
+    
+-----------------------------------------------------------------------    
 """
-
-
-
-
-
